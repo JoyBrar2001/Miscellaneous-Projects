7997ab5857cef5e2be8247aa505a4093f8010cf9
Joy Brar <joybrar2001@gmail.com>
2023-07-03 06:30:46 +0530

    Added Script.js Done with Tabbed Gallery

---
+++ Tabbed Gallery/script.js
@@ -0,0 +1,21 @@
+const allTabs = document.querySelectorAll('.tabs li');
+const allImages = document.querySelectorAll('.images img');
+
+function removeActive(){
+    allTabs.forEach((tab) => {
+        tab.classList.remove('active');
+    });
+    allImages.forEach((img) => {
+        img.classList.remove('active');
+    });
+}
+
+allTabs.forEach((tab,index) => {
+    tab.addEventListener('click', () => {
+        removeActive();
+        tab.classList.add('active');
+        allImages[index].classList.add('active');
+        document.body.style.background = `url(img/bg${index+1}.jpeg) center no-repeat`;
+        document.body.style.backgroundSize = 'cover';
+    });
+})
